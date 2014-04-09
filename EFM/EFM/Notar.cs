e1de1f693ef6605f66b2a2967cac4c84b800dc3a
mnuhodzic1 <mnuhodzic1@etf.unsa.ba>
2014-04-09 22:32:10 +0000

    Osnovni dio klase Notar uraden, kasnije dodati metode

---
+++ EFM/EFM/Notar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFM
+{
+    public class Notar : Osoba
+    {
+        /// <summary>
+        /// Kreira novi objekat tipa Notar
+        /// </summary>
+        /// <param name="Ime">Ime notara</param>
+        /// <param name="Prezime">Prezime notara</param>
+        /// <param name="AdresaStanovanja">Adresa notara</param>
+        /// <param name="BrojTelefona">Broj telefona od notara</param>
+        /// <param name="DatumRodjenja">Datum rodjenja notara</param>
+        /// <param name="BrojLicneKarte">Broj licne karte od notara</param>
+        public Notar(string Ime, string Prezime, string AdresaStanovanja, string BrojTelefona, DateTime DatumRodjenja, string BrojLicneKarte)
+            : base(Ime, Prezime, AdresaStanovanja, BrojTelefona, DatumRodjenja, BrojLicneKarte)
+        {
+
+        }
+
+        /// <summary>
+        /// Konstrukor bez parametara, namjenjen za inicijaliziranje notara
+        /// </summary>
+        public Notar()
+        {
+
+        }
+    }
+}
+
