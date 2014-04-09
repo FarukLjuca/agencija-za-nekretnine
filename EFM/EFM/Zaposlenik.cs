6c8ebfd3f117e8ec11f5724d252c598bb5e4eb43
mnuhodzic1 <mnuhodzic1@etf.unsa.ba>
2014-04-09 22:33:40 +0000

    Klasa Zaposlenik, uraden konstruktor, property

---
+++ EFM/EFM/Zaposlenik.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFM
+{
+    public abstract class Zaposlenik : Osoba
+    {
+    
+    public decimal plata;
+    
+    /// <summary>
+    /// Kreira novi objekat tipa Zaposlenik
+    /// </summary>
+    /// <param name="Ime">Ime zaposlenika</param>
+    /// <param name="Prezime">Prezime zaposlenika</param>
+    /// <param name="AdresaStanovanja">Adresa zaposlenika</param>
+    /// <param name="BrojTelefona">Broj telefona od zaposlenika</param>
+    /// <param name="DatumRodjenja">Datum rodjenja zaposlenika</param>
+    /// <param name="BrojLicneKarte">Broj licne karte od zaposlenika</param>
+    /// <param name="Plata">Plata od zaposlenika</param>
+    /// <exception cref="EFM.Exc.ArgumentException">Izuzetak biva bacen kada je plata negativna</exception>
+    public Zaposlenik(string Ime, string Prezime, string AdresaStanovanja, string BrojTelefona, DateTime DatumRodjenja, string BrojLicneKarte, decimal Plata)
+            : base (Ime, Prezime, AdresaStanovanja, BrojTelefona, DatumRodjenja, BrojLicneKarte)
+        {
+            this.Plata = Plata;
+        }
+
+    /// <summary>
+    /// Plata za zaposlenika
+    /// </summary>
+    public Decimal Plata
+    { 
+        get { return plata;} 
+        set { if (value < 0) throw new Exception("PLata ne moze biti negativna!"); plata = value; } 
+    }
+
+    /// <summary>
+    /// Konstrukor bez parametara, namjenjen za inicijaliziranje zaposlenika
+    /// </summary>
+    public Zaposlenik ()
+        {
+
+        }
+    }
+}
+
