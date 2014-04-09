fd65c1dedf8a08668f27bf697ecc5daa1d9c3444
mnuhodzic1@etf.unsa.ba <mnuhodzic1@etf.unsa.ba@654a21dc-12cc-28b7-4345-0dc84160ed30>
2014-04-09 22:18:39 +0000

    Klasa Cistacica, nasljeduje Zaposlenika. Kasnije cemo trebati dodati metode. Muris

---
+++ trunk/EFM/EFM/Cistacica.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFM
+{
+    public class Cistacica : Zaposlenik
+    {
+        /// <summary>
+        /// Kreira novi objekat tipa Administrator
+        /// </summary>
+        /// <param name="Ime">Ime administratora</param>
+        /// <param name="Prezime">Prezime administratora</param>
+        /// <param name="AdresaStanovanja">Adresa administratora</param>
+        /// <param name="BrojTelefona">Broj telefona od administratora</param>
+        /// <param name="DatumRodjenja">Datum rodjenja administratora</param>
+        /// <param name="BrojLicneKarte">Broj licne karte od administratora</param>
+        public Cistacica(string Ime, string Prezime, string AdresaStanovanja, string BrojTelefona, DateTime DatumRodjenja, string BrojLicneKarte, decimal Plata)
+            : base (Ime, Prezime, AdresaStanovanja, BrojTelefona, DatumRodjenja, BrojLicneKarte, Plata)
+        {
+
+        }
+
+        /// <summary>
+        /// Konstrukor bez parametara, namjenjes iskljucivo za serijalizaciju/deserijalizaciju
+        /// </summary>
+        public Administrator()
+        {
+
+        }
+    }
+}
+
+
+
