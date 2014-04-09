71992aee9cf61ee5e77c264076e13bee8635592c
mnuhodzic1@etf.unsa.ba <mnuhodzic1@etf.unsa.ba@654a21dc-12cc-28b7-4345-0dc84160ed30>
2014-04-09 22:30:32 +0000

    Osnovna implementacija , dakle napisali smo konstruktor i atribute.  Potrebno je kasnije dodati metode.

---
+++ trunk/EFM/EFM/Racunovodja.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFM
+{
+    public class Racunovodja : Zaposlenik
+    {
+        /// <summary>
+        /// Kreira novi objekat tipa zaposlenik koji ima funkciju racunovodje
+        /// </summary>
+        /// <param name="Ime">Ime racunovodje</param>
+        /// <param name="Prezime">Prezime racunovodje</param>
+        /// <param name="AdresaStanovanja">Adresa racunovodje</param>
+        /// <param name="BrojTelefona">Broj telefona od racunovodje</param>
+        /// <param name="DatumRodjenja">Datum rodjenja racunovodje</param>
+        /// <param name="BrojLicneKarte">Broj licne karte od racunovodje</param>
+        public Racunovodja(string Ime, string Prezime, string AdresaStanovanja, string BrojTelefona, DateTime DatumRodjenja, string BrojLicneKarte, decimal Plata)
+            : base (Ime, Prezime, AdresaStanovanja, BrojTelefona, DatumRodjenja, BrojLicneKarte, Plata)
+        {
+
+        }
+
+        /// <summary>
+        /// Konstrukor bez parametara, namjenjen za inicijaliziranje racunovodje
+        /// </summary>
+        public Racunovodja()
+        {
+
+        }
+    }
+}
+
+
+
