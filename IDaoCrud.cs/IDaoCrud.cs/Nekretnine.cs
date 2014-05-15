29188d36ea2545a97bebb35618ec40ea491117f6
mnuhodzic1 <mnuhodzic1@etf.unsa.ba>
2014-05-15 06:52:20 +0000

    Muris DAO, EUgovorDAO, NekretnineDAO

---
+++ IDaoCrud.cs/IDaoCrud.cs/Nekretnine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ZadacaDao
+{
+    public class Nekretnine
+    {
+        long id;
+        string lokacija, tipNekretnine, rezervisanostNekretnine, cistocaNekretnine;
+        int cijenaNekretnine;
+
+        List<Single> ocjene = new List<Single>();
+
+        public Nekretnine(string i, string j, string k, string l, int m)
+        {
+            lokacija = i;
+            tipNekretnine = j;
+            rezervisanostNekretnine = k;
+            cistocaNekretnine = l;
+            cijenaNekretnine = m;
+        }
+
+        public long Id
+        {
+            get;
+            set;
+        }
+        public string Lokacija
+        {
+            get;
+            set;
+        }
+        public string TipNekretnine
+        {
+            get;
+            set;
+        }
+        public string RezervisanostNekretnine
+        {
+            get;
+            set;
+        }
+        public string CistocaNekretnine
+        {
+            get;
+            set;
+        }
+        public int CijenaNekretnine
+        {
+            get;
+            set;
+        }
+
+    }
+}
