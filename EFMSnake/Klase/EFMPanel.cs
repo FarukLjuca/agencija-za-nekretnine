b134f223e07075c8b408aceda414507fd535a961
epajic1 <epajic1@etf.unsa.ba>
2014-06-04 22:23:54 +0000

    EFMPanel dodan

---
+++ EFMSnake/Klase/EFMPanel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFMSnake.Klase
+{
+	class EFMPanel : Panel
+	{
+		public EFMPanel()
+		{
+			this.DoubleBuffered = true;
+			this.SetStyle (ControlStyles.OptimizedDoubleBuffer, true);
+		}
+	}
+}
