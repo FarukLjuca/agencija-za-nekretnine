9b4b06eea78603886c78bb247935b3794af2bac6
epajic1@etf.unsa.ba <epajic1@etf.unsa.ba@654a21dc-12cc-28b7-4345-0dc84160ed30>
2014-06-04 23:03:05 +0000

    Snake 55%

--- trunk/EFMSnake/FrmMain.Designer.cs
+++ trunk/EFMSnake/FrmMain.Designer.cs
@@ -28,120 +28,273 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.gbxIgrac2 = new System.Windows.Forms.GroupBox();
-            this.flpTijelo2 = new System.Windows.Forms.FlowLayoutPanel();
-            this.flpGlava2 = new System.Windows.Forms.FlowLayoutPanel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.flpHrana = new System.Windows.Forms.FlowLayoutPanel();
-            this.label5 = new System.Windows.Forms.Label();
-            this.gbxIgrac1 = new System.Windows.Forms.GroupBox();
-            this.flpTijelo1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.flpGlava1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.imglGlava = new System.Windows.Forms.ImageList(this.components);
-            this.imglTijelo = new System.Windows.Forms.ImageList(this.components);
-            this.imglHrana = new System.Windows.Forms.ImageList(this.components);
-            this.btnStart = new System.Windows.Forms.Button();
-            this.btnAbout = new System.Windows.Forms.Button();
-            this.efmPanel1 = new EFMSnake.Klase.EFMPanel();
-            this.panel1.SuspendLayout();
-            this.gbxIgrac2.SuspendLayout();
-            this.gbxIgrac1.SuspendLayout();
-            this.SuspendLayout();
-            // 
+
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.gbxIgrac2 = new System.Windows.Forms.GroupBox();
+			this.flpTijelo2 = new System.Windows.Forms.FlowLayoutPanel();
+			this.flpGlava2 = new System.Windows.Forms.FlowLayoutPanel();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.flpHrana = new System.Windows.Forms.FlowLayoutPanel();
+			this.label5 = new System.Windows.Forms.Label();
+			this.gbxIgrac1 = new System.Windows.Forms.GroupBox();
+			this.flpTijelo1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.flpGlava1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.imglGlava = new System.Windows.Forms.ImageList(this.components);
+			this.imglTijelo = new System.Windows.Forms.ImageList(this.components);
+			this.imglHrana = new System.Windows.Forms.ImageList(this.components);
+			this.btnStart = new System.Windows.Forms.Button();
+			this.btnAbout = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.efmPanel1 = new Klase.EFMPanel ();
+			this.gbxIgrac2.SuspendLayout();
+			this.gbxIgrac1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)))); 
+
+                        // 
             // panel1
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
-            this.panel1.Controls.Add(this.gbxIgrac2);
-            this.panel1.Controls.Add(this.flpHrana);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.gbxIgrac1);
-            this.panel1.Location = new System.Drawing.Point(12, 215);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(292, 436);
-            this.panel1.TabIndex = 0;
-            // 
-            // gbxIgrac2
-            // 
-            this.gbxIgrac2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+			this.panel1.Controls.Add(this.gbxIgrac2);
+			this.panel1.Controls.Add(this.flpHrana);
+			this.panel1.Controls.Add(this.label5);
+			this.panel1.Controls.Add(this.gbxIgrac1);
+			this.panel1.Location = new System.Drawing.Point(12, 215);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(292, 436);
+			this.panel1.TabIndex = 0;
+			// 
+			// gbxIgrac2
+			// 
+			this.gbxIgrac2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.gbxIgrac2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("gbxIgrac2.BackgroundImage")));
-            this.gbxIgrac2.Controls.Add(this.flpTijelo2);
-            this.gbxIgrac2.Controls.Add(this.flpGlava2);
-            this.gbxIgrac2.Controls.Add(this.label2);
-            this.gbxIgrac2.Controls.Add(this.label3);
-            this.gbxIgrac2.ForeColor = System.Drawing.SystemColors.Control;
-            this.gbxIgrac2.Location = new System.Drawing.Point(159, 3);
-            this.gbxIgrac2.Name = "gbxIgrac2";
-            this.gbxIgrac2.Size = new System.Drawing.Size(127, 277);
-            this.gbxIgrac2.TabIndex = 1;
-            this.gbxIgrac2.TabStop = false;
-            this.gbxIgrac2.Text = "Igrac 2";
-            // 
-            // flpTijelo2
-            // 
-            this.flpTijelo2.AutoScroll = true;
-            this.flpTijelo2.Location = new System.Drawing.Point(9, 167);
-            this.flpTijelo2.Name = "flpTijelo2";
-            this.flpTijelo2.Size = new System.Drawing.Size(90, 90);
-            this.flpTijelo2.TabIndex = 8;
-            // 
-            // flpGlava2
-            // 
-            this.flpGlava2.AutoScroll = true;
-            this.flpGlava2.Location = new System.Drawing.Point(9, 46);
-            this.flpGlava2.Name = "flpGlava2";
-            this.flpGlava2.Size = new System.Drawing.Size(90, 90);
-            this.flpGlava2.TabIndex = 8;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(6, 151);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Odaberite tijelo zmije:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label3.Location = new System.Drawing.Point(6, 30);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(111, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Odaberite glavu zmije:";
-            // 
-            // flpHrana
-            // 
-            this.flpHrana.AutoScroll = true;
-            this.flpHrana.Location = new System.Drawing.Point(96, 299);
-            this.flpHrana.Name = "flpHrana";
-            this.flpHrana.Size = new System.Drawing.Size(90, 90);
-            this.flpHrana.TabIndex = 8;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(93, 283);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(112, 13);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "Odaberite hranu zmije:";
-            // 
-            // gbxIgrac1
-            // 
-            this.gbxIgrac1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this.gbxIgrac2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("gbxIgrac2.BackgroundImage")));
+			this.gbxIgrac2.Controls.Add(this.flpTijelo2);
+			this.gbxIgrac2.Controls.Add(this.flpGlava2);
+			this.gbxIgrac2.Controls.Add(this.label2);
+			this.gbxIgrac2.Controls.Add(this.label3);
+			this.gbxIgrac2.ForeColor = System.Drawing.SystemColors.Control;
+			this.gbxIgrac2.Location = new System.Drawing.Point(159, 3);
+			this.gbxIgrac2.Name = "gbxIgrac2";
+			this.gbxIgrac2.Size = new System.Drawing.Size(127, 277);
+			this.gbxIgrac2.TabIndex = 1;
+			this.gbxIgrac2.TabStop = false;
+			this.gbxIgrac2.Text = "Igrac 2";
+			// 
+			// flpTijelo2
+			// 
+			this.flpTijelo2.AutoScroll = true;
+			this.flpTijelo2.Location = new System.Drawing.Point(9, 167);
+			this.flpTijelo2.Name = "flpTijelo2";
+			this.flpTijelo2.Size = new System.Drawing.Size(90, 90);
+			this.flpTijelo2.TabIndex = 8;
+			// 
+			// flpGlava2
+			// 
+			this.flpGlava2.AutoScroll = true;
+			this.flpGlava2.Location = new System.Drawing.Point(9, 46);
+			this.flpGlava2.Name = "flpGlava2";
+			this.flpGlava2.Size = new System.Drawing.Size(90, 90);
+			this.flpGlava2.TabIndex = 8;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.label2.Location = new System.Drawing.Point(6, 151);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(106, 13);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Odaberite tijelo zmije:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.label3.Location = new System.Drawing.Point(6, 30);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(111, 13);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Odaberite glavu zmije:";
+			// 
+			// flpHrana
+			// 
+			this.flpHrana.AutoScroll = true;
+			this.flpHrana.Location = new System.Drawing.Point(96, 299);
+			this.flpHrana.Name = "flpHrana";
+			this.flpHrana.Size = new System.Drawing.Size(90, 90);
+			this.flpHrana.TabIndex = 8;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(93, 283);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(112, 13);
+			this.label5.TabIndex = 3;
+			this.label5.Text = "Odaberite hranu zmije:";
+			// 
+			// gbxIgrac1
+			// 
+			this.gbxIgrac1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
+
+			this.gbxIgrac1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("gbxIgrac1.BackgroundImage")));
+			this.gbxIgrac1.Controls.Add(this.flpTijelo1);
+			this.gbxIgrac1.Controls.Add(this.flpGlava1);
+			this.gbxIgrac1.Controls.Add(this.label7);
+			this.gbxIgrac1.Controls.Add(this.label1);
+			this.gbxIgrac1.ForeColor = System.Drawing.SystemColors.Control;
+			this.gbxIgrac1.Location = new System.Drawing.Point(3, 3);
+			this.gbxIgrac1.Name = "gbxIgrac1";
+			this.gbxIgrac1.Size = new System.Drawing.Size(140, 277);
+			this.gbxIgrac1.TabIndex = 1;
+			this.gbxIgrac1.TabStop = false;
+			this.gbxIgrac1.Text = "Igrac 1";
+			// 
+			// flpTijelo1
+			// 
+			this.flpTijelo1.AutoScroll = true;
+			this.flpTijelo1.Location = new System.Drawing.Point(9, 167);
+			this.flpTijelo1.Name = "flpTijelo1";
+			this.flpTijelo1.Size = new System.Drawing.Size(90, 90);
+			this.flpTijelo1.TabIndex = 8;
+			// 
+			// flpGlava1
+			// 
+			this.flpGlava1.AutoScroll = true;
+			this.flpGlava1.Location = new System.Drawing.Point(9, 46);
+			this.flpGlava1.Name = "flpGlava1";
+			this.flpGlava1.Size = new System.Drawing.Size(90, 90);
+			this.flpGlava1.TabIndex = 7;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.label7.Location = new System.Drawing.Point(6, 151);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(106, 13);
+			this.label7.TabIndex = 5;
+			this.label7.Text = "Odaberite tijelo zmije:";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.label1.Location = new System.Drawing.Point(6, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(111, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Odaberite glavu zmije:";
+			// 
+			// imglGlava
+			// 
+			this.imglGlava.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imglGlava.ImageStream")));
+			this.imglGlava.TransparentColor = System.Drawing.Color.Transparent;
+			this.imglGlava.Images.SetKeyName(0, "1401929673_ksnake.png");
+			this.imglGlava.Images.SetKeyName(1, "1401929705_head.png");
+			this.imglGlava.Images.SetKeyName(2, "1401929711_pumpkin.png");
+			this.imglGlava.Images.SetKeyName(3, "1401929716_scream.png");
+			this.imglGlava.Images.SetKeyName(4, "1401929721_jack.png");
+			this.imglGlava.Images.SetKeyName(5, "1401929743_Dog_head.png");
+			this.imglGlava.Images.SetKeyName(6, "1401929752_Games.png");
+			this.imglGlava.Images.SetKeyName(7, "1401929773_Cat_head.png");
+			this.imglGlava.Images.SetKeyName(8, "1401929782_Helmet.png");
+			this.imglGlava.Images.SetKeyName(9, "1401929820_spiderman.png");
+			this.imglGlava.Images.SetKeyName(10, "1401929826_boss.png");
+			this.imglGlava.Images.SetKeyName(11, "1401929831_hell_boy.png");
+			this.imglGlava.Images.SetKeyName(12, "1401929835_matrix.png");
+			this.imglGlava.Images.SetKeyName(13, "1401929847_choler.png");
+			// 
+			// imglTijelo
+			// 
+			this.imglTijelo.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imglTijelo.ImageStream")));
+			this.imglTijelo.TransparentColor = System.Drawing.Color.Transparent;
+			this.imglTijelo.Images.SetKeyName(0, "1401929933_519679-186_FormRoundedBorder.png");
+			this.imglTijelo.Images.SetKeyName(1, "1401929935_519898-184_FormRectangleBorder.png");
+			this.imglTijelo.Images.SetKeyName(2, "1401929938_519677-183_FormRectangle.png");
+			this.imglTijelo.Images.SetKeyName(3, "1401929941_519678-185_FormRounded.png");
+			this.imglTijelo.Images.SetKeyName(4, "1401929953_star.png");
+			this.imglTijelo.Images.SetKeyName(5, "1401929954_star.png");
+			this.imglTijelo.Images.SetKeyName(6, "1401929957_118.png");
+			this.imglTijelo.Images.SetKeyName(7, "1401929961_star.png");
+			this.imglTijelo.Images.SetKeyName(8, "1401929985_icon-ios7-circle-outline.png");
+			this.imglTijelo.Images.SetKeyName(9, "1401929988_icon-ios7-circle-filled.png");
+			// 
+			// imglHrana
+			// 
+			this.imglHrana.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imglHrana.ImageStream")));
+			this.imglHrana.TransparentColor = System.Drawing.Color.Transparent;
+			this.imglHrana.Images.SetKeyName(0, "1401930225_teachers_day.png");
+			this.imglHrana.Images.SetKeyName(1, "1401930229_burger.png");
+			this.imglHrana.Images.SetKeyName(2, "1401930232_muffin.png");
+			this.imglHrana.Images.SetKeyName(3, "1401930235_coffee.png");
+			this.imglHrana.Images.SetKeyName(4, "1401930238_Apple.png");
+			this.imglHrana.Images.SetKeyName(5, "1401930241_Cherry Chocolate.png");
+			this.imglHrana.Images.SetKeyName(6, "1401930244_pizza_slice_128.png");
+			this.imglHrana.Images.SetKeyName(7, "1401930248_Strawberry.png");
+			this.imglHrana.Images.SetKeyName(8, "1401930255_cake_9.png");
+			this.imglHrana.Images.SetKeyName(9, "1401930259_french_fries.png");
+			this.imglHrana.Images.SetKeyName(10, "1401930277_cup_ice_cream.png");
+			// 
+			// btnStart
+			// 
+			this.btnStart.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnStart.BackgroundImage")));
+			this.btnStart.ForeColor = System.Drawing.SystemColors.Control;
+			this.btnStart.Location = new System.Drawing.Point(940, 407);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(116, 48);
+			this.btnStart.TabIndex = 1;
+			this.btnStart.Text = "START";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler(this.START_Click);
+			// 
+			// btnAbout
+			// 
+			this.btnAbout.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAbout.BackgroundImage")));
+			this.btnAbout.ForeColor = System.Drawing.SystemColors.Control;
+			this.btnAbout.Location = new System.Drawing.Point(940, 470);
+			this.btnAbout.Name = "btnAbout";
+			this.btnAbout.Size = new System.Drawing.Size(116, 25);
+			this.btnAbout.TabIndex = 2;
+			this.btnAbout.Text = "About";
+			this.btnAbout.UseVisualStyleBackColor = true;
+			this.btnAbout.Click += new System.EventHandler(this.button1_Click_1);
+			// 
+			// FrmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.ClientSize = new System.Drawing.Size(1123, 663);
+			this.Controls.Add(this.btnAbout);
+			this.Controls.Add(this.btnStart);
+			this.Controls.Add(this.panel1);
+			this.KeyPreview = true;
+			this.Name = "FrmMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "EFM Snake";
+			this.Load += new System.EventHandler(this.FrmMain_Load);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.gbxIgrac2.ResumeLayout(false);
+			this.gbxIgrac2.PerformLayout();
+			this.gbxIgrac1.ResumeLayout(false);
+			this.gbxIgrac1.PerformLayout();
+			this.ResumeLayout(false);
+
             this.gbxIgrac1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("gbxIgrac1.BackgroundImage")));
             this.gbxIgrac1.Controls.Add(this.flpTijelo1);
             this.gbxIgrac1.Controls.Add(this.flpGlava1);
@@ -294,6 +447,7 @@
             this.gbxIgrac1.ResumeLayout(false);
             this.gbxIgrac1.PerformLayout();
             this.ResumeLayout(false);
+
 
 		}
 
