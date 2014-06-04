1f5bb077b1f5d22aebcfec4153d1223d7b1d7112
epajic1 <epajic1@etf.unsa.ba>
2014-06-04 17:48:13 +0000

    Evo ga, dodao sam klase Zmija, GlavaZmije, TijeloZmije, HranaZmije, kao i interface IBlock

---
+++ EFMSnake/EFMSnake/EFMSnake/GlavaZmije.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace EFMSnake
+{
+	/// <summary>
+	/// Klasa koja opisuje Glavu Zmije
+	/// </summary>
+	public class GlavaZmije : IBlock
+	{
+		/// <summary>
+		/// Kreira glavu zmije
+		/// </summary>
+		/// <param name="B">Slika glave zmije</param>
+		public GlavaZmije(System.Windows.Media.Imaging.BitmapImage B = null)
+		{
+			Slika = B;
+		}
+		/// <summary>
+		/// Slika glave zmije
+		/// </summary>
+		public System.Windows.Media.Imaging.BitmapImage Slika { get; set; }
+	}
+}
---
+++ EFMSnake/EFMSnake/EFMSnake/HranaZmije.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace EFMSnake
+{
+	/// <summary>
+	/// Klasa koja opisuje Hranu za Zmiju
+	/// </summary>
+	public class HranaZmije : IBlock
+	{
+		/// <summary>
+		/// Kreira hranu za zmiju
+		/// </summary>
+		/// <param name="B">Slika hrane za zmiju</param>
+		public HranaZmije(System.Windows.Media.Imaging.BitmapImage B = null)
+		{
+			Slika = B;
+		}
+		/// <summary>
+		/// Slika hrane za zmiju
+		/// </summary>
+		public System.Windows.Media.Imaging.BitmapImage Slika { get; set; }
+	}
+}
