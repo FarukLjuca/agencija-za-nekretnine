831d04688c54921b691fe6f66b7a2299382bdb95
epajic1@etf.unsa.ba <epajic1@etf.unsa.ba@654a21dc-12cc-28b7-4345-0dc84160ed30>
2014-06-06 17:01:14 +0000

    Dodan DAL, kreiran Home tab (80%) Popunjena LoginForma (izgled samo)

--- trunk/EFM/EFM/DAO/ZaposlenikDAO.cs
+++ trunk/EFM/EFM/DAO/ZaposlenikDAO.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DB = System.Data.SQLite;
 namespace EFM.DAO
 {
 	public class ZaposlenikDAO : IDaoCrud<Zaposlenik>
