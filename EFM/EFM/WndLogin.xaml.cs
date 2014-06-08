4dcfb21fb72a958a2dfed05e9a5b001b02ff6c34
epajic1 <epajic1@etf.unsa.ba>
2014-06-08 15:04:45 +0000

    Vraćeno nešto staro xD

--- EFM/EFM/WndLogin.xaml.cs
+++ EFM/EFM/WndLogin.xaml.cs
@@ -14,24 +14,51 @@ using System.Windows.Shapes;
 
 namespace EFM
 {
-    /// <summary>
-    /// Interaction logic for WndLogin.xaml
-    /// </summary>
-    public partial class WndLogin : Window
-    {
-        public WndLogin()
-        {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Interaction logic for WndLogin.xaml
+	/// </summary>
+	public partial class WndLogin : Window
+	{
+		public WndLogin()
+		{
+			InitializeComponent ();
+			TxtUserID.Focus ();
+		}
+		public Zaposlenik User { get; private set; }
+		public MainWindow.Privilegija Privilegija { get; private set; }
+		private void btnOK_Click_1(object sender, RoutedEventArgs e)
+		{
+			//Sada je dovoljno samo kao user i pass prazno -.-
+			if (TxtUserID.Text == "" && TxtPassword.Text == "")
+			{ this.DialogResult = true; Privilegija = MainWindow.Privilegija.Direktor; User = new Zaposlenik { Ime = "Neko" }; this.Close (); return; }
+			//DAO.ZaposlenikDAO d = new DAO.ZaposlenikDAO ();
+			//Zaposlenik z = new Zaposlenik ();
+			//z.UserName = TxtUserID.Text;
+			//z = d.Read (z);
+			//if (z == null)
+			//{
+			//	tbInfo.Visibility = System.Windows.Visibility.Visible;
+			//	TxtUserID.SelectAll ();
+			//	return;
+			//}
+			//if (z.PassWord != TxtPassword.Text)
+			//{ tbInfo.Visibility = System.Windows.Visibility.Visible; TxtPassword.SelectAll (); return; }
+			//if (z.TIP == "Administrator") Privilegija = MainWindow.Privilegija.Admin;
+			//else if (z.TIP == "Agent") Privilegija = MainWindow.Privilegija.Agent;
+			//else if (z.TIP == "Direktor") Privilegija = MainWindow.Privilegija.Direktor;
+			//else if (z.TIP == "Racunovodja") Privilegija = MainWindow.Privilegija.Racunovodja;
+			//else if (z.TIP == "Cistacica") Privilegija = MainWindow.Privilegija.Cistacica;
+			//User = z;
+			this.DialogResult = true;
+			this.Close ();
 
-        private void btnOK_Click_1(object sender, RoutedEventArgs e)
-        {
+		}
 
-        }
-
-        private void btnCancel_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-    }
+		private void btnCancel_Click_1(object sender, RoutedEventArgs e)
+		{
+			this.DialogResult = false;
+			Application.Current.Shutdown ();
+		}
+	}
 }
+
